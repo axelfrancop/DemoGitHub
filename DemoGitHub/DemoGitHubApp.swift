faa2216d0061e8a3fb4a63320ca5dd1937e4d3cf
axelfrancop <afrancopedroso@gmail.com>
2024-05-22 14:30:01 -0300

    Initial Commit

---
+++ DemoGitHub/DemoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGitHubApp.swift
+//  DemoGitHub
+//
+//  Created by Axel Pedroso on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
